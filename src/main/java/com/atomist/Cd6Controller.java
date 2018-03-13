9e2cc7cee0e2bea9be7079f62ee1dac7c794a89b
Christian Dupuis <cd@atomist.com>
2018-03-13 22:40:01 +0100

    Update Cd6Controller.java

--- src/main/java/com/atomist/Cd6Controller.java
+++ src/main/java/com/atomist/Cd6Controller.java
@@ -11,7 +11,7 @@ class Cd6Controller {
 
     @RequestMapping(method = GET, path = "/")
     public String root() {
-        
+        // 
         // some comment
         return "App running: Served from Meerbusch, Germany";
     }
